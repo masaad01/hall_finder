76ddfe8ea8383b9969057154a1a02309a97501f3
Mohammad <masaad01@hotmail.com>
2022-11-20 20:18:49 +0300

    react

---
+++ client/src/index.jsx
@@ -0,0 +1,7 @@
+//creating react index file
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
