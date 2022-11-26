b83d107a4ce1c7267a7f1a8e9e50bdc069af2ba3
Mohammad <masaad01@hotmail.com>
2022-11-27 00:10:16 +0300

    changed algorithm

--- src/index.ts
+++ src/index.ts
@@ -17,49 +17,82 @@ app.use(express.json());
 app.use(cors())
 
 app.get('/', async (req, res) => {
-  const startTime = req.query.startTime||10;
-  const endTime = req.query.endTime||11;
-  const day = req.query.day||"sun";
+  const CLOSE_TIME = 20;
+  const MAX_WAIT_TIME = 15 / 60; // 15 minutes (until hall free)
+  const time = req.query.time || "12:00";
+  const day = req.query.day || "sun";
   const sections = await sectionRepo.createQueryBuilder("section")
-  .where(`days like '%${day}%'`).getMany();
+    .where(`days like '%${day}%'`).getMany();
   console.time("getFreeAtTimeN");
-  const freeHalls = getFreeAtTimeN(Number(startTime),Number(endTime),sections);
+  const freeHalls = getFreeAtTimeN(timeFromString(String(time)), sections, MAX_WAIT_TIME);
   console.timeEnd("getFreeAtTimeN");
 
-  console.log(freeHalls.length);
-  res.json(freeHalls);
+  console.log(freeHalls.size);
+  const sorted = [...freeHalls.entries()].sort((a, b) => b[1] - a[1]);
+
+  const result = sorted.map(([hall, freeDuration]) => {
+    // if (freeDuration === Infinity) {
+    //   freeDuration = CLOSE_TIME - timeFromString(String(time));
+    // }
+    return {
+      hall,
+      freeDuration: timeToString(freeDuration) === "Invalid time" ? "for the rest of the day" : timeToString(freeDuration),
+    };
+  });
+
+  res.json(result);
 })
 
 
-function getFreeAtTimeN(startTime:number,endTime:number, sections:Section[]){
-    const myMap = new Map<string,boolean>();
-    sections.forEach(section=>{
-      if(myMap.has(section.hall)&&myMap.get(section.hall)&& !hallIsFree(startTime,endTime,section)){
-        myMap.set(section.hall,false);
-      }
-      else{
-        if(hallIsFree(startTime,endTime,section)){
-          myMap.set(section.hall,true);
-        }
-        else{
-          myMap.set(section.hall,false);
-        }
-      }
-      
-    })
-    const freeHalls:string[] = [];
-    myMap.forEach((value,key)=>{
-      if(value){
-        freeHalls.push(key);
-      }
-    })
-    return freeHalls;
-
+function getFreeAtTimeN(time: number, sections: Section[], maxWaitTime = 0) {
+  const myMap = new Map<string, number>();
+  sections.forEach(section => {
+    const freeDuration = hallFreeDuration(time, section);
+    if (myMap.has(section.hall)) {
+      myMap.set(section.hall, Math.min(myMap.get(section.hall)!, freeDuration));
+    }
+    else {
+      myMap.set(section.hall, freeDuration);
+    }
+  })
+  const freeHalls = new Map<string, number>();
+  myMap.forEach((value, key) => {
+    if (value >= -maxWaitTime) {
+      freeHalls.set(key, value);
+    }
+  })
+  return freeHalls;
 }
 
-function hallIsFree(startTime:number,endTime:number,section :Section){
-  const sectionIsNotFree =  (section.startTime >= startTime && section.startTime < endTime || section.endTime > startTime && section.endTime <= endTime)
-  return !sectionIsNotFree;
+function hallFreeDuration(time: number, section: Section) {
+  if (section.startTime > time) {
+    return section.startTime - time;
+  }
+  if (section.endTime <= time) {
+    return Infinity;
+  }
+  return time - section.endTime;
+}
+
+function timeToString(time: number) {
+  // time format: hh:mm
+  // check if time is valid
+  if (time < 0 || time > 24) {
+    return "Invalid time";
+  }
+  const hours = Math.floor(time);
+  const minutes = Math.round((time - hours) * 60);
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+}
+
+function timeFromString(time: string) {
+  // time format: hh:mm
+  // check if time is valid
+  if (!time.match(/^\d{2}:\d{2}$/)) {
+    return -1;
+  }
+  const [hours, minutes] = time.split(":").map(Number);
+  return hours + minutes / 60;
 }
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
